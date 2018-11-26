64faebfcfee51c921b69e0f9ad3605e69d48dd35
Tanner Marsh <returningnull@gmail.com>
2018-11-26 18:05:51 -0500

    Begin adding menu items to database

--- orders/models.py
+++ orders/models.py
@@ -1,15 +1,33 @@
 from django.db import models
 
 # Create your models here.
-class Menu(models.Model):
-    type = models.CharField(max_length=64)
-    name = models.CharField(max_length=64)
-    size = models.CharField(max_length=3)
+class MenuItem(models.Model):
+
+    ITEM_TYPES = (
+        ('RP', 'Regular Pizza'),
+        ('SP', 'Sicilian Pizza'),
+        ('SU', 'Sub'),
+        ('PA', 'Pasta'),
+        ('SA', 'Salad'),
+        ('DP', 'Dinner Platter')
+    )
+    type = models.CharField(max_length=64, choices=ITEM_TYPES)
+
+    name = models.CharField(max_length=64, blank=True)
+
+    ITEM_SIZES = (
+        ('SM', 'Small'),
+        ('LG', 'Large'),
+        ('ONE', 'One Size')
+    )
+    size = models.CharField(max_length=12, choices=ITEM_SIZES)
+
     numToppings = models.IntegerField(blank=True)
+
     cost = models.DecimalField(max_digits=4, decimal_places=2)
 
     def __str__(self):
-        if type == "Pizza":
-            return f"{size} {type} pizza with {numToppings} toppings (${cost})"
+        if self.type == "Regular Pizza" or "Sicilian Pizza":
+            return f"{self.size} {self.type} with {self.numToppings} toppings (${self.cost})"
         else:
-            return "{size} {name} {type} ({cost})"
+            return f"{self.size} {self.name} {self.type} ({self.cost})"
