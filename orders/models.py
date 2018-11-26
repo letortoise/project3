e93f0e9af2158ee39730664b6f02c6c84c4cdc40
Tanner <tanner@Tanners-MacBook-Pro.local>
2018-11-26 17:17:10 -0500

    Created model for menu items

--- orders/models.py
+++ orders/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Menu(models.Model):
+    type = models.CharField(max_length=64)
+    name = models.CharField(max_length=64)
+    size = models.CharField(max_length=5, blank=True)
+    numToppings = models.IntegerField(blank=True)
+    cost = models.DecimalField(max_digits=4, decimal_places=2)
+
+    def __str__(self):
+        if type == "Pizza":
+            return f"{size} {type} pizza with {numToppings} toppings (${cost})"
+        else:
+            return "test"
